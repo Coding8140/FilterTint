7ee5e5f96d48e4485ab9bcdd9ccea2045d9d313b
Coding8140 <83092430+Coding8140@users.noreply.github.com>
2022-02-19 11:22:20 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+function preload()
+{
+
+}
+
+function setup()
+{
+canvas = createCanvas(700,700);
+canvas.position(110,250);
+video = createCapture(VIDEO);
+video.hide();
+tint_color = "";
+}
+
+function draw()
+{
+image(video,0,0,700,700);
+tint(tint_color);
+}
+
+function take_snapshot()
+{
+save('makayla.png');
+}
+
+function filter_tint()
+{
+tint_color = document.getElementById("color_name").value;
+}
